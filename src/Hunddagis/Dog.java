1d9e7db4f8290aa9ab2e88f99b45dbb74837ee96
carl.hummerhielm <carl.hummerhielm@elev.ga.ntig.se>
2024-03-19 08:20:26 +0100

    La till klasserna kopplat till mitt klassdiagram

---
+++ src/Hunddagis/Dog.java
@@ -0,0 +1,8 @@
+package Hunddagis;
+
+public class Dog {
+    private String name;
+    private String breed;
+    private int age;
+    private Owner myOwner;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import Hunddagis.Hunddagis;
+
 public class Main {
     public static void main(String[] args) {
+        new Hunddagis(); //instantierar hunddagiset
         System.out.println("Hello world!");
     }
 }
